4c34cb93c84424fb2b5aa3f83070006fc308b970
mpadge <mark.padgham@email.com>
2021-02-24 14:47:41 +0100

    cp updated R/plumber.R to inst/

--- inst/plumber.R
+++ inst/plumber.R
@@ -4,27 +4,32 @@
 #* @param u The URL for a repo
 #* @post /report
 function (u) {
+
     pkg_name <- tail (strsplit (u, "/") [[1]], 1)
     org <- tail (strsplit (u, "/") [[1]], 2) [1]
 
     cache_dir <- Sys.getenv ("cache_dir")
     repo_updated <- pkgreport::check_cache (org, pkg_name, cache_dir)
+    branch <- get_default_branch (org, pkg_name)
 
-    download_repo <- function (u, pkg_name) {
+    download_repo <- function (u, pkg_name, branch) {
+
         if (substring (u, nchar (u), nchar (u)) == "/")
             u <- substring (u, 1, nchar (u) - 1)
-        u <- paste0 (u, "/archive/master.zip")
+        u <- paste0 (u, "/archive/", branch, ".zip")
 
-        f <- file.path (cache_dir, paste0 (pkg_name, "-master.zip"))
+        f <- file.path (cache_dir, paste0 (pkg_name, "-", branch, ".zip"))
         download.file (u, destfile = f)
         return (f)
     }
 
-    local_repo <- file.path (cache_dir, paste0 (pkg_name, "-master"))
+    local_repo <- file.path (cache_dir, paste0 (pkg_name, "-", branch, ""))
     local_zip <- paste0 (local_repo, ".zip")
+
     if (!file.exists (local_zip) | repo_updated) {
+
         message ("downloading repo ...")
-        f <- download_repo (u, pkg_name)
+        f <- download_repo (u, pkg_name, branch)
     }
 
     flist <- unzip (local_zip, exdir = cache_dir)
@@ -39,5 +44,6 @@ function (u) {
 
 #* @get /mean
 getmean <- function (n = 10){
+
     return (mean (rnorm (n)))
 }
