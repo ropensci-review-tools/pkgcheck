93c1030634b3df1b1cca0d27266d32451f974fd8
mpadge <mark.padgham@email.com>
2021-02-24 14:34:00 +0100

    separate get_default_branch fn

--- R/github.R
+++ R/github.R
@@ -66,7 +66,7 @@ commits_qry <- function (gh_cli, org, repo, branch = "main") {
     return (qry)
 }
 
-get_latest_commit <- function (org, repo) {
+get_default_branch <- function (org, repo) {
 
     token <- get_gh_token ()
 
@@ -79,6 +79,20 @@ get_latest_commit <- function (org, repo) {
     x <- gh_cli$exec(qry$queries$default_branch) %>%
         jsonlite::fromJSON ()
     branch <- x$data$repository$defaultBranchRef$name
+
+    return (branch)
+}
+
+get_latest_commit <- function (org, repo) {
+
+    token <- get_gh_token ()
+
+    gh_cli <- ghql::GraphqlClient$new (
+        url = "https://api.github.com/graphql",
+        headers = list (Authorization = paste0 ("Bearer ", token))
+    )
+
+    branch <- get_default_branch (org, repo)
 
     qry <- commits_qry (gh_cli, org = org, repo = repo, branch = branch)
     x <- gh_cli$exec(qry$queries$get_commits) %>%
