03382c728a3684724889dbc7a68e9859a8b257aa
mpadge <mark.padgham@email.com>
2021-09-03 15:15:49 +0200

    pkg_has_scrap has to return something, not NULL

--- R/misc-checks.R
+++ R/misc-checks.R
@@ -352,7 +352,7 @@ pkg_has_scrap <- function (path) {
                               error = function (e) NULL)
 
     if (is.null (all_contents))
-        return (NULL)
+        return (character (0)) # not NULL!
 
     all_contents <- vapply (decompose_path (all_contents),
                             function (i) utils::tail (i, 1L),
