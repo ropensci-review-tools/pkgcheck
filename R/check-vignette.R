60277b64ae0479b85932064de44625ac208c2f96
mpadge <mark.padgham@email.com>
2021-09-17 12:21:05 +0200

    add some space to check-vignette

--- R/check-vignette.R
+++ R/check-vignette.R
@@ -4,26 +4,30 @@
 #' @return Logical flag
 #' @noRd
 pkgchk_has_vignette <- function (path) {
-  # from pkgdown https://github.com/r-lib/pkgdown/blob/705ff7c650bb1c7d46d35e72f27ad093689e2f29/R/package.r#L202
-  base <- file.path(path, "vignettes")
 
-  if (!dir.exists(base)) {
-    vig_path <- character()
-  } else {
-    vig_path <- dir(base, pattern = "\\.[rR]md$")
-  }
-  vig_path <- basename(vig_path)
-  vig_path <- vig_path[!grepl("^_", basename(vig_path))]
+    # from pkgdown https://github.com/r-lib/pkgdown/blob/705ff7c650bb1c7d46d35e72f27ad093689e2f29/R/package.r#L202
+    base <- file.path(path, "vignettes")
 
-  is_html <- function(vignette_path) {
-    format <- rmarkdown::default_output_format(file.path("vignettes", vignette_path))$name
-    # FIXME: Maybe some output formats are HTML without that name pattern?
-    grepl("html", name)
-  }
-  is_html <- unlist(lapply(vig_path, is_html))
-  vig_path <- vig_path[is_html]
+    if (!dir.exists(base)) {
+        vig_path <- character()
+    } else {
+        vig_path <- dir(base, pattern = "\\.[rR]md$")
+    }
 
-  return (length (vig_path) >= 1L)
+    vig_path <- basename(vig_path)
+    vig_path <- vig_path[!grepl("^_", basename(vig_path))]
+
+    is_html <- function(vignette_path) {
+
+        format <- rmarkdown::default_output_format(file.path("vignettes", vignette_path))$name
+        # FIXME: Maybe some output formats are HTML without that name pattern?
+        grepl("html", name)
+    }
+
+    is_html <- unlist(lapply(vig_path, is_html))
+    vig_path <- vig_path[is_html]
+
+    return (length (vig_path) >= 1L)
 
 }
 
