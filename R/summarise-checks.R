763e71a607fa75229003f30c6bfd18837693042b
mpadge <mark.padgham@email.com>
2021-09-17 13:00:09 +0200

    include @maelle's vignette check in summarise_all_checks fn
    That's the only missing bit; now it all works. Thank you\!

--- R/summarise-checks.R
+++ R/summarise-checks.R
@@ -21,6 +21,7 @@ summarise_all_checks <- function (checks) {
               summarise_has_citation (checks),
               summarise_has_codemeta (checks),
               summarise_fns_have_exs (checks),
+              summarise_has_vignette (checks),
               summarise_left_assign_chk (checks),
               summarise_url_bugs (checks, "has_url"),
               summarise_url_bugs (checks, "has_bugs"),
