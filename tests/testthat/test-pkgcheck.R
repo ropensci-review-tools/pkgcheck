6f7672e6247a20bd843f00fbbd34932973e711a7
mpadge <mark.padgham@email.com>
2021-09-28 22:28:15 +0200

    restructure snapshot test
    @maelle the snapshot was flaky because it included all rcmdcheck output which differs
    on different systems, often for very minor reasons like compliation flags. This commit
    ditches all rcmdcheck results from snapshot tests, which is invasive enough that i
    modified your previous envvar approach to the brutal dissection fn in tests themselves.
    All seems much more stable now :smile:

--- tests/testthat/test-pkgcheck.R
+++ tests/testthat/test-pkgcheck.R
@@ -1,7 +1,6 @@
 test_that("pkgcheck", {
   withr::local_envvar(list("PKGCHECK_SRR_REPORT_FILE" = "report.html"))
   withr::local_envvar(list("PKGCHECK_TEST_NETWORK_FILE" = "network.html"))
-  withr::local_envvar(list("PKGCHECK_TESTING" = "yep"))
 
   options (repos = c (CRAN = "https://cloud.r-project.org"))
 
@@ -31,10 +30,6 @@ test_that("pkgcheck", {
   expect_true (all (items %in% names (chk$info)))
 
   md <- checks_to_markdown (chk, render = FALSE)
-  md_dir <- withr::local_tempdir()
-  writeLines(md, con = file.path(md_dir, "checks.md"))
-
-  testthat::expect_snapshot_file(file.path(md_dir, "checks.md"))
 
   a <- attributes (md)
   expect_true (length (a) > 0L)
@@ -42,4 +37,45 @@ test_that("pkgcheck", {
     "is_noteworthy",
     "network_file",
     "srr_report_file") %in% names (a)))
+
+  # *****************************************************************
+  # ***********************   SNAPSHOT TEST   ***********************
+  # *****************************************************************
+  #
+  # some checks like rcmdcheck differ on different systems for things like
+  # compilation flags, so the snapshot test excludes any rmcdcheck output. It
+  # also reverts the final package versions to a generic number.
+  edit_markdown <- function (md) {
+
+      rcmd <- grep ("R CMD check", md) [1:2] # summary items
+      gp <- grep ("`goodpractice` results", md)
+      end_fold <- grep ("<\\/details>", md)
+      # then -3 to keep ["", "</p>", "</details>"]:
+      gp <- seq (gp, end_fold [end_fold > gp] [1] - 3)
+
+      md <- md [-c (rcmd, gp)]
+
+      change_pkg_vers <- function (md, pkg = "pkgstats", to = "42") {
+          i <- grep ("Package Versions", md)
+          pkg_i <- grep (pkg, md)
+          pkg_i <- pkg_i [pkg_i > i] [1]
+          md [pkg_i] <- gsub ("([0-9]\\.)+[0-9]+", to, md [pkg_i])
+          # white space also changes with version numbers:
+          md [pkg_i] <- gsub (paste0 (to, "\\s+"),
+                              paste0 (to, "    "), md [pkg_i])
+          return (md)
+      }
+      md <- change_pkg_vers (md, "pkgstats")
+      md <- change_pkg_vers (md, "pkgcheck")
+      md <- change_pkg_vers (md, "srr")
+
+      return (md)
+  }
+  md <- edit_markdown (md)
+
+  md_dir <- withr::local_tempdir()
+  writeLines(md, con = file.path(md_dir, "checks.md"))
+
+  testthat::expect_snapshot_file(file.path(md_dir, "checks.md"))
+
 })
