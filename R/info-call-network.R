e5645364df2c29e4092229423697ab933decf007
mpadge <mark.padgham@email.com>
2021-09-29 18:03:37 +0200

    mv fn_call_network fn from pkgcheck-fn.R -> info-call-network.R

---
+++ R/info-call-network.R
@@ -0,0 +1,51 @@
+
+#' Plot function call network from \pkg{pkgstats} objects
+#'
+#' @param s Result of \pkg{pkgstats} call
+#' @return Local path to 'visjs' HTML diagram of call network.
+#' @noRd
+fn_call_network <- function (s) {
+
+    if (nrow (s$stats$network) == 0L && nrow (s$stats$objects) == 0L)
+        return (NULL)
+
+    visjs_dir <- file.path (getOption ("pkgcheck.cache_dir"),
+                             "static")
+    if (!dir.exists (visjs_dir))
+        dir.create (visjs_dir, recursive = TRUE)
+
+    visjs_file <- paste0 (s$out$package,
+                          "_pkgstats",
+                          substring (s$out$git$HEAD, 1, 8),
+                          ".html")
+    visjs_path <- file.path (visjs_dir, visjs_file)
+
+    # clean up any older ones
+    flist <- list.files (visjs_dir,
+                         pattern = paste0 (s$out$package, "_pkgstats"),
+                         full.names = TRUE)
+
+    if (!visjs_path %in% flist) {
+
+        unlink (flist, recursive = TRUE)
+        pkgstats::plot_network (s$stats, vis_save = visjs_path)
+        # visNetwork renames the generic `lib` folder to the specific name, so
+        # needs to be cleaned up:
+        flist <- list.files (visjs_dir,
+                             pattern = paste0 (s$out$package, "_pkgstats"),
+                             full.names = TRUE)
+        libdir <- flist [which (dir.exists (flist))]
+        if (!"lib" %in% list.files (visjs_dir)) {
+            if (length (libdir) > 0) {
+                libdir <- libdir [1]
+                fpath <- file.path (libdir, "..")
+                newlibdir <- file.path (normalizePath (fpath), "lib")
+                file.rename (libdir, newlibdir)
+            }
+        } else {
+            unlink (libdir)
+        }
+    }
+
+    return (visjs_path)
+}
--- R/pkgcheck-fn.R
+++ R/pkgcheck-fn.R
@@ -160,52 +160,6 @@ fmt_pkgstats_info <- function (s) {
     return (stat_chks)
 }
 
-fn_call_network <- function (s) {
-
-    if (nrow (s$stats$network) == 0L && nrow (s$stats$objects) == 0L)
-        return (NULL)
-
-    visjs_dir <- file.path (getOption ("pkgcheck.cache_dir"),
-                             "static")
-    if (!dir.exists (visjs_dir))
-        dir.create (visjs_dir, recursive = TRUE)
-
-    visjs_file <- paste0 (s$out$package,
-                          "_pkgstats",
-                          substring (s$out$git$HEAD, 1, 8),
-                          ".html")
-    visjs_path <- file.path (visjs_dir, visjs_file)
-
-    # clean up any older ones
-    flist <- list.files (visjs_dir,
-                         pattern = paste0 (s$out$package, "_pkgstats"),
-                         full.names = TRUE)
-
-    if (!visjs_path %in% flist) {
-
-        unlink (flist, recursive = TRUE)
-        pkgstats::plot_network (s$stats, vis_save = visjs_path)
-        # visNetwork renames the generic `lib` folder to the specific name, so
-        # needs to be cleaned up:
-        flist <- list.files (visjs_dir,
-                             pattern = paste0 (s$out$package, "_pkgstats"),
-                             full.names = TRUE)
-        libdir <- flist [which (dir.exists (flist))]
-        if (!"lib" %in% list.files (visjs_dir)) {
-            if (length (libdir) > 0) {
-                libdir <- libdir [1]
-                fpath <- file.path (libdir, "..")
-                newlibdir <- file.path (normalizePath (fpath), "lib")
-                file.rename (libdir, newlibdir)
-            }
-        } else {
-            unlink (libdir)
-        }
-    }
-
-    return (visjs_path)
-}
-
 version_info <- function (nosrr) {
 
     pkgs <- c ("pkgstats", "pkgcheck")
