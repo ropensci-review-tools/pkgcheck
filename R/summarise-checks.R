44f7f776e98aecc042c2e171029457075debe8ce
mpadge <mark.padgham@email.com>
2021-09-29 10:01:52 +0200

    pass main check obj to summarise_has_vignette

--- R/summarise-checks.R
+++ R/summarise-checks.R
@@ -21,7 +21,7 @@ summarise_all_checks <- function (checks) {
               summarise_has_citation (checks),
               summarise_has_codemeta (checks),
               summarise_fns_have_exs (checks),
-              summarise_has_vignette (checks$checks),
+              summarise_has_vignette (checks),
               summarise_left_assign_chk (checks$info),
               summarise_url_bugs (checks$checks, "has_url"),
               summarise_url_bugs (checks$checks, "has_bugs"),
