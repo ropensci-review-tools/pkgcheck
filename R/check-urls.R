64880c7912c6eed44f22fd071a30f5a8b2e92d1b
mpadge <mark.padgham@email.com>
2021-09-29 17:57:30 +0200

    mv url_exists fn from utils.R -> check-urls.R

--- R/check-urls.R
+++ R/check-urls.R
@@ -42,3 +42,72 @@ output_pkgchk_has_bugs <- function (checks) {
 
     return (out)
 }
+
+#' Bob Rudis's URL checker function
+#'
+#' @param x a single URL
+#' @param non_2xx_return_value what to do if the site exists but the HTTP status
+#' code is not in the `2xx` range. Default is to return `FALSE`.
+#' @param quiet if not `FALSE`, then every time the `non_2xx_return_value`
+#' condition arises a warning message will be displayed. Default is `FALSE`.
+#' @param ... other params (`timeout()` would be a good one) passed directly to
+#' `httr::HEAD()` and/or `httr::GET()`
+#'
+#' @note
+#' https://stackoverflow.com/questions/52911812/check-if-url-exists-in-r
+#' @noRd
+url_exists <- function(x, non_2xx_return_value = FALSE, quiet = FALSE, ...) {
+
+  # you don't need thse two functions if you're already using `purrr`
+  # but `purrr` is a heavyweight compiled pacakge that introduces
+  # many other "tidyverse" dependencies and this doesnt.
+
+  capture_error <- function(code, otherwise = NULL, quiet = TRUE) {
+    tryCatch(
+      list(result = code, error = NULL),
+      error = function(e) {
+        if (!quiet)
+          message("Error: ", e$message)
+
+        list(result = otherwise, error = e)
+      },
+      interrupt = function(e) {
+        stop("Terminated by user", call. = FALSE)
+      }
+    )
+  }
+
+  safely <- function(.f, otherwise = NULL, quiet = TRUE) {
+    function(...) capture_error(.f(...), otherwise, quiet)
+  }
+
+  sHEAD <- safely(httr::HEAD)   # nolint
+  sGET <- safely(httr::GET)     # nolint
+
+  # Try HEAD first since it's lightweight
+  res <- sHEAD(x, ...)
+
+  if (is.null(res$result) ||
+      ((httr::status_code(res$result) %/% 200) != 1)) {
+
+    res <- sGET(x, ...)
+
+    if (is.null (res$result))
+        return (NA) # or whatever you want to return on "hard" errors
+
+    if (((httr::status_code(res$result) %/% 200) != 1)) {
+      if (!quiet)
+          warning (paste0 ("Requests for [",
+                           x,
+                           "] responded but without an HTTP status ",
+                           "code in the 200-299 range"))
+      return (non_2xx_return_value)
+    }
+
+    return(TRUE)
+
+  } else {
+    return(TRUE)
+  }
+
+}
