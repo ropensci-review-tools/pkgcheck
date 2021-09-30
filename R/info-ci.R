0a086d66cb4c5812f08f5fed204c7c372b8cdd34
mpadge <mark.padgham@email.com>
2021-09-30 21:00:49 +0200

    spaceout R/info-ci.R

--- R/info-ci.R
+++ R/info-ci.R
@@ -7,9 +7,9 @@
 #' @return Character vector of hyperlinked badge images
 #' @noRd
 pkgchk_ci_badges <- function (u) {
-
-    if (!curl::has_internet ())
-        return (NULL)
+    if (!curl::has_internet ()) {
+          return (NULL)
+      }
 
     orgrepo <- strsplit (u, "\\/") [[1]]
     org <- utils::tail (orgrepo, 2) [1]
@@ -19,68 +19,78 @@ pkgchk_ci_badges <- function (u) {
     branch <- get_default_branch (org, repo)
 
     if (grepl ("github", u)) {
-
-        u_readme <- paste0 ("https://raw.githubusercontent.com/",
-                            org,
-                            "/",
-                            repo,
-                            "/",
-                            branch,
-                            "/README.md")
-
+        u_readme <- paste0 (
+            "https://raw.githubusercontent.com/",
+            org,
+            "/",
+            repo,
+            "/",
+            branch,
+            "/README.md"
+        )
     } else if (grepl ("gitlab", u)) {
-
-        u_readme <- paste0 ("https://gitlab.com/",
-                            org,
-                            "/",
-                            repo,
-                            "/-/raw/",
-                            branch,
-                            "/README.md")
+        u_readme <- paste0 (
+            "https://gitlab.com/",
+            org,
+            "/",
+            repo,
+            "/-/raw/",
+            branch,
+            "/README.md"
+        )
     }
 
-    if (!url_exists (u_readme, quiet = TRUE))
-        return (NULL)
+    if (!url_exists (u_readme, quiet = TRUE)) {
+          return (NULL)
+      }
 
     f <- tempfile (fileext = ".md")
     chk <- utils::download.file (u_readme, destfile = f, quiet = TRUE) # nolint
     readme <- readLines (f, encoding = "UTF-8")
 
-    badges <- unlist (regmatches (readme,
-                                  gregexpr ("https.*\\.svg", readme)))
-    if (length (badges) == 0)
-        return (NULL)
+    badges <- unlist (regmatches (
+        readme,
+        gregexpr ("https.*\\.svg", readme)
+    ))
+    if (length (badges) == 0) {
+          return (NULL)
+      }
     platforms <- c ("github", "travis", "gitlab")
-    badges <- badges [grep (paste0 (platforms, collapse = "|"),
-                            badges)]
+    badges <- badges [grep (
+        paste0 (platforms, collapse = "|"),
+        badges
+    )]
     for (p in platforms) {
-
         index <- grep (p, badges)
         p_u <- p
 
         if (p == "github") {
-
-            wf_nms <- vapply (badges [index], function (i)
-                              utils::tail (strsplit (i, "/") [[1]], 2) [1],
-                              character (1),
-                              USE.NAMES = FALSE)
-            p_u <- paste0 ("https://github.com/",
-                           org,
-                           "/",
-                           repo,
-                           "/actions")
+            wf_nms <- vapply (badges [index], function (i) {
+                  utils::tail (strsplit (i, "/") [[1]], 2) [1]
+              },
+            character (1),
+            USE.NAMES = FALSE
+            )
+            p_u <- paste0 (
+                "https://github.com/",
+                org,
+                "/",
+                repo,
+                "/actions"
+            )
         } else if (p == "travis") {
-
             p_u <- gsub ("\\.svg$", "", badges [index])
         }
 
-        badges [index] <- paste0 ("[![",
-                                  p,
-                                  "](",
-                                  badges [index],
-                                  ")](",
-                                  p_u,
-                                  ")")
+        badges [index] <- paste0 (
+            "[![",
+            p,
+            "](",
+            badges [index],
+            ")](",
+            p_u,
+            ")"
+        )
     }
 
     return (badges)
@@ -94,10 +104,10 @@ pkgchk_ci_badges <- function (u) {
 #' name, the 'conclusion' status, the git 'sha', and the date.
 #' @noRd
 ci_results_gh <- function (path) {
-
     d <- data.frame (read.dcf (file.path (path, "DESCRIPTION")))
-    if (!"URL" %in% names (d))
-        return ("Error: Description has no URL")
+    if (!"URL" %in% names (d)) {
+          return ("Error: Description has no URL")
+      }
 
     u <- strsplit (d$URL, "\\s+") [[1]]
     u <- u [grep ("^https://github\\.com", u)]
@@ -105,38 +115,48 @@ ci_results_gh <- function (path) {
     org <- utils::tail (url, 2) [1]
     repo <- utils::tail (url, 1)
 
-    url <- paste0 ("https://api.github.com/repos/",
-                   org,
-                   "/",
-                   repo,
-                   "/actions/runs")
+    url <- paste0 (
+        "https://api.github.com/repos/",
+        org,
+        "/",
+        repo,
+        "/actions/runs"
+    )
 
     runs <- httr::GET (url) %>% httr::content ()
 
-    if (!"total_count" %in% names (runs))
-        return (NULL)
+    if (!"total_count" %in% names (runs)) {
+          return (NULL)
+      }
 
-    if (runs$total_count == 0)
-        return (NULL)
+    if (runs$total_count == 0) {
+          return (NULL)
+      }
 
     dat <- lapply (runs$workflow_runs, function (i) {
-                       # in-progress runs have no conclusion entry:
-                       concl <- i$conclusion
-                       if (is.null (concl))
-                           concl <- ""
-                   data.frame (name = i$name,
-                               status = i$status,
-                               conclusion = concl,
-                               sha = i$head_sha,
-                               time = i$created_at)
-                   })
+        # in-progress runs have no conclusion entry:
+        concl <- i$conclusion
+        if (is.null (concl)) {
+              concl <- ""
+          }
+        data.frame (
+            name = i$name,
+            status = i$status,
+            conclusion = concl,
+            sha = i$head_sha,
+            time = i$created_at
+        )
+    })
     dat <- do.call (rbind, dat)
     dat$time <- strptime (dat$time, "%Y-%m-%dT%H:%M:%SZ")
     dat$time_dbl <- as.double (dat$time)
     # non-dply group_by %>% summarise:
-    dat <- lapply (split (dat, f = as.factor (dat$name)),
-                   function (i)
-                       i [which.max (i$time_dbl), ])
+    dat <- lapply (
+        split (dat, f = as.factor (dat$name)),
+        function (i) {
+              i [which.max (i$time_dbl), ]
+          }
+    )
     dat <- do.call (rbind, dat)
 
     dat$sha <- substring (dat$sha, 1, 6)
