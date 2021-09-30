6bd3345acfbe8f1d902238ad20865a39285493db
mpadge <mark.padgham@email.com>
2021-09-30 17:12:30 +0200

    spaceout R/check-covr.R

--- R/check-covr.R
+++ R/check-covr.R
@@ -1,30 +1,26 @@
 
 output_pkgchk_covr <- function (checks) {
-
-    out <- list (check_pass = TRUE,
-                summary = "",
-                print = "")
+    out <- list (
+        check_pass = TRUE,
+        summary = "",
+        print = ""
+    )
 
     if (methods::is (checks$goodpractice$covr, "try-error")) {
-
         out$check_pass <- FALSE
         out$summary <- "Package coverage failed"
-
     } else {
-
         coverage <- round (checks$goodpractice$covr$pct_by_line, digits = 1)
 
         if (coverage < 75) {
-
             out$check_pass <- FALSE
-            out$summary <- paste0 ("Package coverage is ",
-                                   coverage,
-                                   "% (should be at least 75%).")
-
+            out$summary <- paste0 (
+                "Package coverage is ",
+                coverage,
+                "% (should be at least 75%)."
+            )
         } else {
-
             out$summary <- paste0 ("Package coverage is ", coverage, "%.")
-
         }
     }
 
