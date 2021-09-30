61a4395a8aed500cdde3422b8983dfd0b07c6ca1
mpadge <mark.padgham@email.com>
2021-09-30 21:02:07 +0200

    spaceout R/info-pkgstats.R

--- R/info-pkgstats.R
+++ R/info-pkgstats.R
@@ -2,23 +2,26 @@
 # actually being checks
 
 pkginfo_url_from_desc <- function (path, type = "URL") {
-
     type <- match.arg (type, c ("URL", "BugReports"))
 
     desc <- file.path (path, "DESCRIPTION")
-    if (!file.exists (desc))
-        return (character (0))
+    if (!file.exists (desc)) {
+          return (character (0))
+      }
 
     d <- data.frame (read.dcf (desc))
-    if (!type %in% names (d))
-        return (character (0))
+    if (!type %in% names (d)) {
+          return (character (0))
+      }
 
     u <- strsplit (d [[type]], "\\s+") [[1]]
     u <- grep ("^https", u, value = TRUE)
-    if (length (u) > 1)
-        u <- grep ("git", u, value = TRUE)
-    if (length (u) > 1)
-        u <- u [which (!grepl ("\\.io", u))]
+    if (length (u) > 1) {
+          u <- grep ("git", u, value = TRUE)
+      }
+    if (length (u) > 1) {
+          u <- u [which (!grepl ("\\.io", u))]
+      }
 
     u <- gsub (",|\\s+", "", u)
 
@@ -40,35 +43,34 @@ pkginfo_pkg_license <- function (s) {
 }
 
 pkginfo_pkgstats_summary <- function (s) {
-
     pkgstats <- fmt_pkgstats_info (s)
 
     num_exported_fns <- pkgstats$value [pkgstats$measure == "n_fns_r_exported"]
     num_non_exported_fns <- pkgstats$value [pkgstats$measure ==
-                                            "n_fns_r_not_exported"]
+        "n_fns_r_not_exported"]
     num_src_fns <- sum (pkgstats$value [pkgstats$measure %in%
-                        c ("n_fns_src", "n_fns_inst")])
+        c ("n_fns_src", "n_fns_inst")])
     loc_exported_fns <- pkgstats$value [pkgstats$measure ==
-                                        "loc_per_fn_r_exp"]
+        "loc_per_fn_r_exp"]
     loc_non_exported_fns <- pkgstats$value [pkgstats$measure ==
-                                            "loc_per_fn_r_not_exp"]
+        "loc_per_fn_r_not_exp"]
     loc_src_fns <- stats::median (pkgstats$value [pkgstats$measure %in%
-                                  c ("loc_per_fn_src", "loc_per_fn_inst")])
+        c ("loc_per_fn_src", "loc_per_fn_inst")])
     num_params_per_fn <- pkgstats$value [pkgstats$measure ==
-                                         "num_params_per_fn"]
+        "num_params_per_fn"]
 
     list (
-         num_authors = s$desc$aut,
-         num_vignettes = unname (s$vignettes [1]),
-         num_data = unname (s$data_stats [1]),
-         imported_pkgs = length (strsplit (s$desc$imports, ",") [[1]]),
-         num_exported_fns = as.integer (num_exported_fns),
-         num_non_exported_fns = as.integer (num_non_exported_fns),
-         num_src_fns = as.integer (num_src_fns),
-         loc_exported_fns = as.integer (loc_exported_fns),
-         loc_non_exported_fns = as.integer (loc_non_exported_fns),
-         loc_src_fns = as.integer (loc_src_fns),
-         num_params_per_fn = as.integer (num_params_per_fn),
-         languages = attr (pkgstats, "language")
-         )
+        num_authors = s$desc$aut,
+        num_vignettes = unname (s$vignettes [1]),
+        num_data = unname (s$data_stats [1]),
+        imported_pkgs = length (strsplit (s$desc$imports, ",") [[1]]),
+        num_exported_fns = as.integer (num_exported_fns),
+        num_non_exported_fns = as.integer (num_non_exported_fns),
+        num_src_fns = as.integer (num_src_fns),
+        loc_exported_fns = as.integer (loc_exported_fns),
+        loc_non_exported_fns = as.integer (loc_non_exported_fns),
+        loc_src_fns = as.integer (loc_src_fns),
+        num_params_per_fn = as.integer (num_params_per_fn),
+        languages = attr (pkgstats, "language")
+    )
 }
