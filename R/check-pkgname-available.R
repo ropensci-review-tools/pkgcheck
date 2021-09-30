c327c822d61c4e145019f57e5bac970a043f324f
mpadge <mark.padgham@email.com>
2021-09-30 17:17:35 +0200

    spaceout R/check-pkgname-available.R [ci skip]

--- R/check-pkgname-available.R
+++ R/check-pkgname-available.R
@@ -5,37 +5,37 @@
 #' \pkg{goodpractice} results.
 #' @noRd
 pkgchk_pkgname_available <- function (checks) {
-
-    desc <- data.frame (read.dcf (file.path (checks$package$path,
-                                             "DESCRIPTION")))
+    desc <- data.frame (read.dcf (file.path (
+        checks$package$path,
+        "DESCRIPTION"
+    )))
     pkg <- desc$Package
 
-    pkg_grepped <- grep (.standard_regexps()$valid_package_name,
-                         pkg,
-                         value = TRUE)
+    pkg_grepped <- grep (
+        .standard_regexps ()$valid_package_name,
+        pkg,
+        value = TRUE
+    )
 
     ap <- data.frame (utils::available.packages ())
 
-    return (!pkg %in% ap$Package &
-            pkg == pkg_grepped)
+    return (
+        !pkg %in% ap$Package &
+        pkg == pkg_grepped)
 }
 
 output_pkgchk_pkgname <- function (checks) {
-
-    out <- list (check_pass = TRUE,
-                summary = "",
-                print = "") # no print method
+    out <- list (
+        check_pass = TRUE,
+        summary = "",
+        print = ""
+    ) # no print method
 
     if (checks$checks$pkgname_available & !checks$checks$on_cran) {
-
         out$summary <- "Package name is available"
-
     } else if (checks$checks$on_cran) {
-
         out$summary <- "Package is already on CRAN."
-
     } else {
-
         out$check_pass <- FALSE
         out$summary <- "Package name is not available (on CRAN)."
     }
