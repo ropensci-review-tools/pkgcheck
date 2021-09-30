47292ac5b387e660e90674185b3b53a11f8f6896
mpadge <mark.padgham@email.com>
2021-09-30 20:48:08 +0200

    spaceout R/check-scrap.R

--- R/check-scrap.R
+++ R/check-scrap.R
@@ -11,36 +11,49 @@ pkgchk_has_scrap <- function (checks) {
     # Have to tryCatch because gert errors anywhere other than a git repo. This
     # means scrap can only be detected in git repos.
     all_contents <- tryCatch (gert::git_ls (repo = checks$package$path)$path,
-                              error = function (e) NULL)
+        error = function (e) NULL
+    )
 
-    if (is.null (all_contents))
-        return (character (0)) # not NULL!
+    if (is.null (all_contents)) {
+          return (character (0))
+      } # not NULL!
 
-    all_contents <- vapply (decompose_path (all_contents),
-                            function (i) utils::tail (i, 1L),
-                            character (1))
+    all_contents <- vapply (
+        decompose_path (all_contents),
+        function (i) utils::tail (i, 1L),
+        character (1)
+    )
 
-    scrap <- function() paste0 (c ("^\\.DS_Store$",
-                                   "^Thumbs.db$",
-                                   "^\\.vscode$",
-                                   "\\.o$"),
-                                collapse = "|")
+    scrap <- function () {
+        paste0 (c (
+            "^\\.DS_Store$",
+            "^Thumbs.db$",
+            "^\\.vscode$",
+            "\\.o$"
+        ),
+        collapse = "|"
+        )
+    }
 
     return (grep (scrap (), all_contents, value = TRUE))
 }
 
 output_pkgchk_has_scrap <- function (checks) {
-
-    out <- list (check_pass = length (checks$checks$scrap) == 0L,
-                summary = "",
-                print = "")
+    out <- list (
+        check_pass = length (checks$checks$scrap) == 0L,
+        summary = "",
+        print = ""
+    )
 
     if (!out$check_pass) {
-
         out$summary <- "Package contains unexpected files."
-        out$print <- list (message = paste0 ("Package contains the ",
-                                             "following unexpected files:"),
-                           obj = checks$checks$scrap)
+        out$print <- list (
+            message = paste0 (
+                "Package contains the ",
+                "following unexpected files:"
+            ),
+            obj = checks$checks$scrap
+        )
     }
 
     return (out)
