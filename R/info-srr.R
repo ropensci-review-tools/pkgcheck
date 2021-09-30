63fc021a4e952f49721cf2fdddaa7b63a16a9f10
mpadge <mark.padgham@email.com>
2021-09-30 21:02:44 +0200

    spaceout R/info-srr.R

--- R/info-srr.R
+++ R/info-srr.R
@@ -22,18 +22,21 @@
 #'
 #' @noRd
 pkginfo_srr_report <- function (path) {
-
     srr <- tryCatch (
-                srr::srr_stats_pre_submit (path, quiet = TRUE),
-                error = function (e) e)
+        srr::srr_stats_pre_submit (path, quiet = TRUE),
+        error = function (e) e
+    )
 
-    if (is.null (srr))
-        return (NULL) # Not an srr package
+    if (is.null (srr)) {
+          return (NULL)
+      } # Not an srr package
 
     srr_okay <- FALSE
     if (!methods::is (srr, "error") &
-        any (grepl ("^All applicable standards \\[v.+\\] have been documented",
-                    srr))) {
+        any (grepl (
+            "^All applicable standards \\[v.+\\] have been documented",
+            srr
+        ))) {
         srr_okay <- TRUE
     }
 
@@ -41,38 +44,46 @@ pkginfo_srr_report <- function (path) {
 
     # get path to report in cache dir:
     pkg_hash <- current_hash (path)
-    static_dir <- file.path (Sys.getenv ("PKGCHECK_CACHE_DIR"),
-                             "static")
+    static_dir <- file.path (
+        Sys.getenv ("PKGCHECK_CACHE_DIR"),
+        "static"
+    )
     f <- paste0 (pkg_hash [1], "_srr", pkg_hash [2])
     srr_report_file <- file.path (static_dir, paste0 (f, ".html"))
     srr_md_file <- file.path (static_dir, paste0 (f, ".md"))
 
     flist <- list.files (static_dir,
-                         full.names = TRUE)
-    fnames <- vapply (decompose_path (flist), function (i)
-                      utils::tail (i, 1),
-                      character (1))
+        full.names = TRUE
+    )
+    fnames <- vapply (
+        decompose_path (flist), function (i) {
+              utils::tail (i, 1)
+          },
+        character (1)
+    )
     flist <- flist [grep (f, fnames)]
 
     if (srr_md_file %in% flist) {
-
         srr_rep <- readLines (srr_md_file)
-
     } else {
+        if (length (flist) > 0) {
+              file.remove (flist)
+          }
 
-        if (length (flist) > 0)
-            file.remove (flist)
-
-        srr_rep <- srr::srr_report (path = path,
-                                    view = FALSE)
+        srr_rep <- srr::srr_report (
+            path = path,
+            view = FALSE
+        )
         srr_file_from <- attr (srr_rep, "file")
-        if (!file.copy (srr_file_from, srr_report_file))
-            warning ("srr html file not copied!")
+        if (!file.copy (srr_file_from, srr_report_file)) {
+              warning ("srr html file not copied!")
+          }
         # srr_report stored the .md as a .Rmd in tempdir():
         srr_rmd <- tools::file_path_sans_ext (srr_file_from)
         srr_rmd <- paste0 (srr_rmd, ".Rmd")
-        if (!file.copy (srr_rmd, srr_md_file))
-            warning ("srr md file not copied!")
+        if (!file.copy (srr_rmd, srr_md_file)) {
+              warning ("srr md file not copied!")
+          }
     }
 
     categories <- srr_categories_from_report (srr_rep)
@@ -80,51 +91,54 @@ pkginfo_srr_report <- function (path) {
 
     i <- grep ("standards \\[v.*\\] are missing from your code", srr)
     if (length (i) > 0) {
-
         stds <- srr [-seq (i)]
         blank <- which (nchar (stds) == 0)
-        #srr_tail <- NULL
+        # srr_tail <- NULL
         if (length (blank) > 1) {
             stds_end <- blank [which (diff (blank) > 1) + 1]
-            #srr_tail <- stds [seq (stds_end, length (stds))]
+            # srr_tail <- stds [seq (stds_end, length (stds))]
             stds <- stds [-seq (stds_end, length (stds))]
         }
         stds <- paste0 (stds [which (nchar (stds) > 0)],
-                        collapse = ", ")
+            collapse = ", "
+        )
     }
 
     srr_okay <- length (i) == 0
 
-    list (message = srr,
-          categories = categories,
-          missing_stds = stds,
-          report_file = srr_report_file,
-          okay = srr_okay)
+    list (
+        message = srr,
+        categories = categories,
+        missing_stds = stds,
+        report_file = srr_report_file,
+        okay = srr_okay
+    )
 }
 
 #' Extract the statistical categories from the `srr` report
 #' @param s Result of main `srr:srr_report()` function
 #' @noRd
 srr_categories_from_report <- function (s) {
-
     cats <- regmatches (s, gregexpr ("[A-Z]+[0-9]+\\.[0-9]+([a-z]?)", s))
     cats <- sort (unique (unlist (cats)))
     cats <- unique (gsub ("[0-9].*$", "", cats))
     cats <- cats [which (!cats == "G")]
 
     prefixes <- c ("BS", "EA", "ML", "RE", "SP", "TS", "UL")
-    categories <- c ("Bayesian and Monte Carlo",
-                     "Exploratory Data Analysis",
-                     "Machine Learning",
-                     "Regression and Supervised Learning",
-                     "Spatial",
-                     "Time Series",
-         "Dimensionality Reduction, Clustering and Unsupervised Learning")
+    categories <- c (
+        "Bayesian and Monte Carlo",
+        "Exploratory Data Analysis",
+        "Machine Learning",
+        "Regression and Supervised Learning",
+        "Spatial",
+        "Time Series",
+        "Dimensionality Reduction, Clustering and Unsupervised Learning"
+    )
     cats <- cats [which (cats %in% prefixes)]
 
     return (categories [match (cats, prefixes)])
 }
 
-report_file <- function(checks) {
-    Sys.getenv("PKGCHECK_SRR_REPORT_FILE", checks$info$srr$report_file)
+report_file <- function (checks) {
+    Sys.getenv ("PKGCHECK_SRR_REPORT_FILE", checks$info$srr$report_file)
 }
