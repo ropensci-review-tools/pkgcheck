00ba4faeabd19df10062e679ac78bb2c02289919
mpadge <mark.padgham@email.com>
2021-09-30 20:51:03 +0200

    spaceout R/check-urls.R

--- R/check-urls.R
+++ R/check-urls.R
@@ -1,44 +1,50 @@
 
 pkgchk_has_url <- function (checks) {
-
     out <- length (checks$package$url) > 0L
-    if (out)
-        out <- !is.na (checks$package$url)
+    if (out) {
+          out <- !is.na (checks$package$url)
+      }
 
     return (out)
 }
 
 pkgchk_has_bugs <- function (checks) {
-
     out <- length (checks$package$BugReports) > 0L
-    if (out)
-        out <- !is.na (checks$package$BugReports)
+    if (out) {
+          out <- !is.na (checks$package$BugReports)
+      }
 
     return (out)
 }
 
 output_pkgchk_has_url <- function (checks) {
+    out <- list (
+        check_pass = checks$checks$has_url,
+        summary = "",
+        print = ""
+    ) # no print method
 
-    out <- list (check_pass = checks$checks$has_url,
-                summary = "",
-                print = "") # no print method
-
-    out$summary <- paste0 ("'DESCRIPTION' ",
-                           ifelse (out$check_pass, "has", "does not have"),
-                           " a URL field.")
+    out$summary <- paste0 (
+        "'DESCRIPTION' ",
+        ifelse (out$check_pass, "has", "does not have"),
+        " a URL field."
+    )
 
     return (out)
 }
 
 output_pkgchk_has_bugs <- function (checks) {
+    out <- list (
+        check_pass = checks$checks$has_bugs,
+        summary = "",
+        print = ""
+    ) # no print method
 
-    out <- list (check_pass = checks$checks$has_bugs,
-                summary = "",
-                print = "") # no print method
-
-    out$summary <- paste0 ("'DESCRIPTION' ",
-                           ifelse (out$check_pass, "has", "does not have"),
-                           " a BugReports field.")
+    out$summary <- paste0 (
+        "'DESCRIPTION' ",
+        ifelse (out$check_pass, "has", "does not have"),
+        " a BugReports field."
+    )
 
     return (out)
 }
@@ -56,58 +62,60 @@ output_pkgchk_has_bugs <- function (checks) {
 #' @note
 #' https://stackoverflow.com/questions/52911812/check-if-url-exists-in-r
 #' @noRd
-url_exists <- function(x, non_2xx_return_value = FALSE, quiet = FALSE, ...) {
+url_exists <- function (x, non_2xx_return_value = FALSE, quiet = FALSE, ...) {
 
-  # you don't need thse two functions if you're already using `purrr`
-  # but `purrr` is a heavyweight compiled pacakge that introduces
-  # many other "tidyverse" dependencies and this doesnt.
+    # you don't need thse two functions if you're already using `purrr`
+    # but `purrr` is a heavyweight compiled pacakge that introduces
+    # many other "tidyverse" dependencies and this doesnt.
 
-  capture_error <- function(code, otherwise = NULL, quiet = TRUE) {
-    tryCatch(
-      list(result = code, error = NULL),
-      error = function(e) {
-        if (!quiet)
-          message("Error: ", e$message)
+    capture_error <- function (code, otherwise = NULL, quiet = TRUE) {
+        tryCatch (
+            list (result = code, error = NULL),
+            error = function (e) {
+                if (!quiet) {
+                      message ("Error: ", e$message)
+                  }
 
-        list(result = otherwise, error = e)
-      },
-      interrupt = function(e) {
-        stop("Terminated by user", call. = FALSE)
-      }
-    )
-  }
-
-  safely <- function(.f, otherwise = NULL, quiet = TRUE) {
-    function(...) capture_error(.f(...), otherwise, quiet)
-  }
-
-  sHEAD <- safely(httr::HEAD)   # nolint
-  sGET <- safely(httr::GET)     # nolint
-
-  # Try HEAD first since it's lightweight
-  res <- sHEAD(x, ...)
-
-  if (is.null(res$result) ||
-      ((httr::status_code(res$result) %/% 200) != 1)) {
-
-    res <- sGET(x, ...)
-
-    if (is.null (res$result))
-        return (NA) # or whatever you want to return on "hard" errors
-
-    if (((httr::status_code(res$result) %/% 200) != 1)) {
-      if (!quiet)
-          warning (paste0 ("Requests for [",
-                           x,
-                           "] responded but without an HTTP status ",
-                           "code in the 200-299 range"))
-      return (non_2xx_return_value)
+                list (result = otherwise, error = e)
+            },
+            interrupt = function (e) {
+                stop ("Terminated by user", call. = FALSE)
+            }
+        )
     }
 
-    return(TRUE)
+    safely <- function (.f, otherwise = NULL, quiet = TRUE) {
+        function (...) capture_error (.f (...), otherwise, quiet)
+    }
 
-  } else {
-    return(TRUE)
-  }
+    sHEAD <- safely (httr::HEAD) # nolint
+    sGET <- safely (httr::GET) # nolint
 
+    # Try HEAD first since it's lightweight
+    res <- sHEAD (x, ...)
+
+    if (is.null (res$result) ||
+        ((httr::status_code (res$result) %/% 200) != 1)) {
+        res <- sGET (x, ...)
+
+        if (is.null (res$result)) {
+              return (NA)
+          } # or whatever you want to return on "hard" errors
+
+        if (((httr::status_code (res$result) %/% 200) != 1)) {
+            if (!quiet) {
+                  warning (paste0 (
+                      "Requests for [",
+                      x,
+                      "] responded but without an HTTP status ",
+                      "code in the 200-299 range"
+                  ))
+              }
+            return (non_2xx_return_value)
+        }
+
+        return (TRUE)
+    } else {
+        return (TRUE)
+    }
 }
