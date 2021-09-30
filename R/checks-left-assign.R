d742ed2d4a04521ec00b8704e4157d562e1e1c09
mpadge <mark.padgham@email.com>
2021-09-30 20:56:55 +0200

    spaceout R/checks-left-assign.R

--- R/checks-left-assign.R
+++ R/checks-left-assign.R
@@ -11,43 +11,58 @@
 #' @return Named vector of 2 values tallying instances of usage of `<-` and `=`.
 #' @noRd
 pkgchk_left_assign <- function (checks) {
-
     rdir <- file.path (checks$package$path, "R")
-    if (!file.exists (rdir))
-        return (list (global = FALSE,
-                      usage = c ("<-" = 0L,
-                                 "=" = 0L)))
+    if (!file.exists (rdir)) {
+          return (list (
+              global = FALSE,
+              usage = c (
+                  "<-" = 0L,
+                  "=" = 0L
+              )
+          ))
+      }
 
     rdir <- normalizePath (rdir)
     flist <- list.files (rdir,
-                         full.names = TRUE,
-                         pattern = "\\.q$|\\.r$|\\.s$",
-                         ignore.case = TRUE)
+        full.names = TRUE,
+        pattern = "\\.q$|\\.r$|\\.s$",
+        ignore.case = TRUE
+    )
 
     assigns <- vapply (flist, function (i) {
-                           p <- tryCatch (utils::getParseData (parse (i)),
-                                          error = function (e) NULL)
-                           assigns <- c (":=" = 0L,
-                                         "<-" = 0L,
-                                         "<<-" = 0L,
-                                         "=" = 0L)
-                           if (is.null (p))
-                               return (assigns)
-                           la <- table (p$text [which (p$token ==
-                                                       "LEFT_ASSIGN")])
-                           if (":=" %in% names (la))
-                               assigns [1] <- la [which (names (la) == ":=")]
-                           if ("<-" %in% names (la))
-                               assigns [2] <- la [which (names (la) == "<-")]
-                           if ("<<-" %in% names (la))
-                               assigns [3] <- la [which (names (la) == "<<-")]
-                           if ("=" %in% names (la))
-                               assigns [4] <- la [which (names (la) == "=")]
+        p <- tryCatch (utils::getParseData (parse (i)),
+            error = function (e) NULL
+        )
+        assigns <- c (
+            ":=" = 0L,
+            "<-" = 0L,
+            "<<-" = 0L,
+            "=" = 0L
+        )
+        if (is.null (p)) {
+              return (assigns)
+          }
 
-                           return (assigns)
-                         },
-                         integer (4),
-                         USE.NAMES = TRUE)
+        la <- table (p$text [which (p$token == "LEFT_ASSIGN")])
+
+        if (":=" %in% names (la)) {
+              assigns [1] <- la [which (names (la) == ":=")]
+          }
+        if ("<-" %in% names (la)) {
+              assigns [2] <- la [which (names (la) == "<-")]
+          }
+        if ("<<-" %in% names (la)) {
+              assigns [3] <- la [which (names (la) == "<<-")]
+          }
+        if ("=" %in% names (la)) {
+              assigns [4] <- la [which (names (la) == "=")]
+          }
+
+        return (assigns)
+    },
+    integer (4),
+    USE.NAMES = TRUE
+    )
     assigns <- rowSums (assigns)
     # rm `:=`:
     assigns <- assigns [which (!names (assigns) == ":=")]
@@ -60,30 +75,35 @@ pkgchk_left_assign <- function (checks) {
 }
 
 output_pkgchk_global_assign <- function (checks) {
+    out <- list (
+        check_pass = !checks$checks$left_assign$global,
+        summary = "",
+        print = ""
+    ) # no print method
 
-    out <- list (check_pass = !checks$checks$left_assign$global,
-                summary = "",
-                print = "") # no print method
-
-    if (!out$check_pass)
-        out$summary <- " Package uses global assignment operator ('<<-')."
+    if (!out$check_pass) {
+          out$summary <- " Package uses global assignment operator ('<<-')."
+      }
 
     return (out)
 }
 
 output_pkgchk_left_assign <- function (checks) {
-
     la <- checks$checks$left_assign$usage # tally of [`<-`, `=`]
 
-    out <- list (check_pass = any (la == 0),
-                summary = "",
-                print = "") # no print method
+    out <- list (
+        check_pass = any (la == 0),
+        summary = "",
+        print = ""
+    ) # no print method
 
-    if (!out$check_pass)
-        out$summary <- paste0 ("Package uses inconsistent assignment ",
-                               "operators (",
-                               la [names (la) == "<-"], " '<-' and ",
-                               la [names (la) == "="], " '=').")
+    if (!out$check_pass) {
+          out$summary <- paste0 (
+              "Package uses inconsistent assignment operators (",
+              la [names (la) == "<-"], " '<-' and ",
+              la [names (la) == "="], " '=')."
+          )
+      }
 
     return (out)
 }
