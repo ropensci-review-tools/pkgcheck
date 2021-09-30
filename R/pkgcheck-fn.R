d82d8efae8ff3fc1d0f2cb3f77e7efe20d05083f
mpadge <mark.padgham@email.com>
2021-09-30 21:04:32 +0200

    spaceout R/pkgcheck-fn.R

--- R/pkgcheck-fn.R
+++ R/pkgcheck-fn.R
@@ -10,30 +10,34 @@
 #' @family pkgcheck_fns
 #' @export
 pkgcheck <- function (path = ".", extra_env = .GlobalEnv) {
-
     options (pkgcheck_extra_env = extra_env)
 
     path <- convert_path (path)
 
-    if (checks_running_in_bg (path))
-        stop ("Checks are still running in background process.")
+    if (checks_running_in_bg (path)) {
+          stop ("Checks are still running in background process.")
+      }
 
     # Ensure that ctags works properly (#54):
-    if (interactive ())
-        if (!suppressMessages (pkgstats::ctags_test ())) {
-
-            stop ("The 'pkgstats' package requires 'ctags' which does ",
+    if (interactive ()) {
+          if (!suppressMessages (pkgstats::ctags_test ())) {
+              stop (
+                  "The 'pkgstats' package requires 'ctags' which does ",
                   "not seem to be installed correctly.\nSee ",
                   "https://docs.ropensci.org/pkgstats/#installation",
-                  " for details on how to install 'ctags'.")
-        }
+                  " for details on how to install 'ctags'."
+              )
+          }
+      }
 
     s <- pkgstats_info (path)
 
     checks <- pkgcheck_object ()
-    checks$package <- s$out [c ("package", "path", "version", "url",
-                                "BugReports", "license", "summary",
-                                "dependencies")]
+    checks$package <- s$out [c (
+        "package", "path", "version", "url",
+        "BugReports", "license", "summary",
+        "dependencies"
+    )]
     names (checks$package) [1] <- "name"
 
     checks$info <- s$out [c ("git", "srr", "pkgstats")]
@@ -66,11 +70,12 @@ pkgcheck <- function (path = ".", extra_env = .GlobalEnv) {
 }
 
 pkgcheck_object <- function () {
-
-    out <- list (package = NULL,
-                 info = NULL,
-                 checks = NULL,
-                 meta = NULL)
+    out <- list (
+        package = NULL,
+        info = NULL,
+        checks = NULL,
+        meta = NULL
+    )
 
     class (out) <- append ("pkgcheck", class (out))
 
@@ -78,21 +83,22 @@ pkgcheck_object <- function () {
 }
 
 checks_running_in_bg <- function (path) {
-
     stopvar <- Sys.getenv ("PKGCHECK_PXBG_STOP")
-    if (Sys.getenv ("PKGCHECK_BG") != "")
-        stopvar <- ""
+    if (Sys.getenv ("PKGCHECK_BG") != "") {
+          stopvar <- ""
+      }
 
     logfiles <- logfile_names (path)
-    stopfile <- gsub ("\\_stdout$", "_stop",
-                      logfiles$stdout)
+    stopfile <- gsub (
+        "\\_stdout$", "_stop",
+        logfiles$stdout
+    )
 
     return (stopvar == stopfile &&
-            !file.exists (stopfile))
+        !file.exists (stopfile))
 }
 
 pkgstats_info <- function (path) {
-
     s <- suppressWarnings (pkgstats::pkgstats (path))
     s$path <- path
 
@@ -113,8 +119,10 @@ pkgstats_info <- function (path) {
 
     out$pkgstats <- fmt_pkgstats_info (s)
 
-    return (list (stats = s,
-                  out = out))
+    return (list (
+        stats = s,
+        out = out
+    ))
 }
 
 #' Parse items of the "desc" part of `pkgstats` output
@@ -122,17 +130,21 @@ pkgstats_info <- function (path) {
 #' @param s Result of `pkgstats::pkgstats()` call.
 #' @noRd
 parse_pkg_deps <- function (s) {
-
     fields <- c ("depends", "imports", "suggests", "linking_to")
 
-    d <- lapply (fields, function (i)
-                 cbind (i,
-                        strsplit (s$desc [[i]], ",\\s*") [[1]]))
+    d <- lapply (fields, function (i) {
+          cbind (
+              i,
+              strsplit (s$desc [[i]], ",\\s*") [[1]]
+          )
+      })
 
     d <- do.call (rbind, d)
 
-    data.frame (type = d [, 1],
-                package = d [, 2])
+    data.frame (
+        type = d [, 1],
+        package = d [, 2]
+    )
 }
 
 #' Format \pkg{pkgstats} data
@@ -140,15 +152,14 @@ parse_pkg_deps <- function (s) {
 #' @return Report as formatted string
 #' @noRd
 fmt_pkgstats_info <- function (s) {
-
     s_summ <- pkgstats::pkgstats_summary (s)
     attr (s_summ, "path") <- s$path
     stat_chks <- stats_checks (s_summ)
     languages <- attr (stat_chks, "language")
     # ignore large numbers of files:
     stat_chks$noteworthy [grepl ("^files\\_", stat_chks$measure) &
-                          stat_chks$percentile > 0.5] <- FALSE
-    #is_noteworthy <- any (stat_chks$noteworthy)
+        stat_chks$percentile > 0.5] <- FALSE
+    # is_noteworthy <- any (stat_chks$noteworthy)
     stat_chks$percentile <- 100 * stat_chks$percentile
     stat_chks$noteworthy [which (!stat_chks$noteworthy)] <- ""
 
@@ -164,14 +175,14 @@ fmt_pkgstats_info <- function (s) {
 #' @return The contents of the "checks" items of the main `pkgcheck` object.
 #' @noRd
 collate_checks <- function (checks) {
-
     pkg_fns <- ls (envir = asNamespace ("pkgcheck"))
     check_fns <- grep ("^pkgchk\\_", pkg_fns, value = TRUE)
     exclude_these <- "ci\\_badges|srr"
     check_fns <- check_fns [which (!grepl (exclude_these, check_fns))]
 
-    res <- lapply (check_fns, function (i)
-                          do.call (i, list (checks)))
+    res <- lapply (check_fns, function (i) {
+          do.call (i, list (checks))
+      })
     names (res) <- gsub ("^pkgchk\\_", "", check_fns)
 
     extra_chks <- collate_extra_env_checks (checks)
@@ -180,39 +191,46 @@ collate_checks <- function (checks) {
 }
 
 collate_extra_env_checks <- function (checks) {
-
     extra_env <- options ("pkgcheck_extra_env") [[1]]
-    if (is.null (extra_env))
-        return (NULL)
+    if (is.null (extra_env)) {
+          return (NULL)
+      }
 
-    if (!is.list (extra_env))
-        extra_env <- list (extra_env)
+    if (!is.list (extra_env)) {
+          extra_env <- list (extra_env)
+      }
 
     chks <- lapply (extra_env, function (i) {
-                        i <- env2namespace (i) # in R/utils.R
-                        fns <- grep ("^pkgchk\\_", ls (i), value = TRUE)
-                        out <- lapply (fns, function (j)
-                                       do.call (j, list (checks), envir = i))
-                        names (out) <- gsub ("^pkgchk\\_", "", fns)
+        i <- env2namespace (i) # in R/utils.R
+        fns <- grep ("^pkgchk\\_", ls (i), value = TRUE)
+        out <- lapply (fns, function (j) {
+              do.call (j, list (checks), envir = i)
+          })
+        names (out) <- gsub ("^pkgchk\\_", "", fns)
 
-                        return (out)
-                          })
+        return (out)
+    })
     chks <- unlist (chks)
 
-    not_empty <- vapply (chks,
-                         function (i) length (i) > 0L,
-                         logical (1))
+    not_empty <- vapply (
+        chks,
+        function (i) length (i) > 0L,
+        logical (1)
+    )
 
     return (chks [which (not_empty)])
 }
 
 version_info <- function (nosrr) {
-
     pkgs <- c ("pkgstats", "pkgcheck")
-    if (!nosrr)
-        pkgs <- c (pkgs, "srr")
+    if (!nosrr) {
+          pkgs <- c (pkgs, "srr")
+      }
 
-    vapply (pkgs, function (i)
-            paste0 (utils::packageVersion (i)),
-            character (1))
+    vapply (
+        pkgs, function (i) {
+              paste0 (utils::packageVersion (i))
+          },
+        character (1)
+    )
 }
