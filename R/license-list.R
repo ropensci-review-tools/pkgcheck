c5912276327acc701a6008a1adafaa17927db2a3
mpadge <mark.padgham@email.com>
2021-11-17 13:11:08 +0100

    add license-list files for #30

---
+++ R/license-list.R
@@ -0,0 +1,46 @@
+# do not edit by hand; created by:
+# source ('./data-raw/license-list.r')
+#
+# rOpenSci recommendations:
+# https://devguide.ropensci.org/building.html#licence
+# links to cran list used to fill this license-list at
+# https://svn.r-project.org/R/trunk/share/licenses/license.db
+# plus OSI list which includes the extra CDDL-1.0 license.
+
+license_list <- function () {
+    c (
+        "GPL",
+        "LGPL",
+        "AGPL",
+        "Apache License",
+        "Artistic License",
+        "CeCILL",
+        "FreeBSD",
+        "MIT",
+        "BSD",
+        "BSD_2_clause",
+        "BSD_3_clause",
+        "MPL",
+        "CPL",
+        "EPL",
+        "Lucent Public License",
+        "EUPL",
+        "CC BY-SA 2.0",
+        "CC BY 3.0",
+        "CC BY-SA 3.0",
+        "CC BY-NC 3.0",
+        "CC BY-NC-ND 3.0 US",
+        "CC BY-NC-SA 3.0",
+        "CC BY-SA 3.0 US",
+        "CC BY 4.0",
+        "CC BY-SA 4.0",
+        "CC BY-NC 4.0",
+        "CC BY-NC-SA 4.0",
+        "CC BY-NC-ND 4.0",
+        "CC0",
+        "Zlib",
+        "BSL",
+        "ACM",
+        "CDDL-1.0"
+    )
+}
---
+++ data-raw/license-list.R
@@ -0,0 +1,58 @@
+
+u <- "https://svn.r-project.org/R/trunk/share/licenses/license.db"
+f <- file.path (tempdir (), "license.db")
+download.file (u, f)
+
+x <- readLines (f)
+
+index <- rep (0, length (x))
+index [which (nchar (x) == 0L)] <- 1
+index <- cumsum (index)
+x <- lapply (
+    split (x, f = factor (index)),
+    function (i) i [which (nchar (i) > 0L)]
+)
+licenses <- vapply (x, function (i) {
+    nm <- gsub (
+        "^Name:\\s?", "",
+        grep ("^Name\\:", i, value = TRUE)
+    )
+    ab <- gsub (
+        "^Abbrev:\\s?", "",
+        grep ("^Abbrev\\:", i, value = TRUE)
+    )
+    if (length (ab) == 0L) {
+        ab <- NA_character_
+    }
+    c (name = nm, abbrev = ab)
+},
+character (2),
+USE.NAMES = FALSE
+)
+licenses <- data.frame (t (licenses))
+names (licenses) <- c ("name", "abbrev")
+index <- which (is.na (licenses$abbrev))
+licenses$abbrev [index] <- licenses$name [index]
+licenses <- unique (licenses$abbrev)
+licenses <- paste0 ("        \"", licenses, "\",")
+
+f <- c (
+    "# do not edit by hand; created by:",
+    "# source ('./data-raw/license-list.r')",
+    "#",
+    "# rOpenSci recommendations:",
+    "# https://devguide.ropensci.org/building.html#licence",
+    "# links to cran list used to fill this license-list at",
+    "# https://svn.r-project.org/R/trunk/share/licenses/license.db",
+    "# plus OSI list which includes the extra CDDL-1.0 license.",
+    "",
+    "license_list <- function () {",
+    "    c (",
+    licenses,
+    "        \"CDDL-1.0\"",
+    "    )",
+    "}"
+)
+
+here <- rprojroot::find_root (rprojroot::is_r_package)
+writeLines (f, file.path (here, "R", "license-list.R"))
