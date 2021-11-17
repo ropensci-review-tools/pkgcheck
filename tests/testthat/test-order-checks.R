66ce809ffd36b5b5bad5b826bcc8a1304fb8d488
mpadge <mark.padgham@email.com>
2021-11-17 14:32:24 +0100

    add test-order-checks.R

---
+++ tests/testthat/test-order-checks.R
@@ -0,0 +1,18 @@
+
+# New checks have to be added to the `order_checks` function in `summarise-checks.R`.
+# This test ensures that they are added by failing if not.
+
+test_that ("order checks", {
+
+    pkg_env <- asNamespace ("pkgcheck")
+    pkg_fns <- ls (pkg_env)
+
+    fns <- gsub (
+        "^output\\_pkgchk\\_", "",
+        grep ("^output\\_pkgchk\\_", pkg_fns, value = TRUE)
+    )
+
+    checks <- order_checks (fns)
+
+    expect_equal (length (fns), length (checks))
+})
