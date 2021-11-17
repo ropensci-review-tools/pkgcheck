684aa2eda5e34739e6534b1024b692732c53a3bf
mpadge <mark.padgham@email.com>
2021-11-17 14:00:05 +0100

    pkgcheck_pkgdown for > 1 group

--- R/check-pkgdown.R
+++ R/check-pkgdown.R
@@ -1,8 +1,9 @@
 
 output_pkgchk_pkgdown <- function (checks) {
 
+    # Grouped concepts must mean at least 2, so check is > 1:
     out <- list (
-        check_pass = length (checks$info$pkgdown_concepts) > 0L,
+        check_pass = length (checks$info$pkgdown_concepts) > 1L,
         summary = "",
         print = ""
     )
