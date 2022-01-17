fa3396aaab10c2dbe58167eaee1ff0bdca2d72e4
mpadge <mark.padgham@email.com>
2022-01-17 18:33:37 +0100

    finish PR from @maelle

--- R/read-books.R
+++ R/read-books.R
@@ -3,17 +3,19 @@
 #' @param which Whether to read the released or "dev" development version.
 #'
 #' @export
-read_pkg_guide <- function(which = c("release", "dev")){
-  which <- rlang::arg_match(which, values = c("release", "dev"))
+read_pkg_guide <- function (which = c ("release", "dev")) {
+    which <- match.arg (which [1],
+        c ("release", "dev"),
+        several.ok = FALSE
+    )
 
-  utils::browseURL(sprintf("%sbuilding.html", devguide_url(which)))
+    utils::browseURL (sprintf ("%sbuilding.html", devguide_url (which)))
 }
 
 
-devguide_url <- function(which){
-  switch(
-    which,
-    release = "https://devguide.ropensci.org/",
-    dev = "https://devdevguide.netlify.app/"
-  )
+devguide_url <- function (which) {
+    switch (which,
+        release = "https://devguide.ropensci.org/",
+        dev = "https://devdevguide.netlify.app/"
+    )
 }
