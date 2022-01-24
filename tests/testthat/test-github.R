c850705c84d74647333878cf767d3c7739eb4a03
mpadge <mark.padgham@email.com>
2022-01-24 17:00:06 +0100

    another tweak to fix failing paths on #125

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -15,8 +15,11 @@ expect_error(use_github_action_pkgcheck(dir = dir, inputs = list(notaninput = 23
 # Success
 # macOS GitHub runners append "/private" to paths when they are actually created:
 gha_path <-  gsub ("^\\/private", "", use_github_action_pkgcheck(dir = dir))
+# Constructed paths also use one double-separator between machine and Rtemp dirs:
+gha_path <- gsub ("\\/\\/", "/", gha_path)
 expect_equal(gha_path, path)
 gha_path <- gsub ("^\\/private", "", use_github_action_pkgcheck(dir = dir, overwrite = TRUE))
+gha_path <- gsub ("\\/\\/", "/", gha_path)
 expect_equal(gha_path, path)
 expect_snapshot_file(path)
 expect_snapshot_file(use_github_action_pkgcheck(dir = dir, file_name = "with_inputs.yaml", inputs = list(`post-to-issue` = "true", `summary-only` = "false", ref = "main")))
