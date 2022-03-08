a622c0577e46a56df7ca5308b89032b230f4e3f3
mpadge <mark.padgham@email.com>
2022-03-09 10:29:40 +1100

    test yaml branch param for #135

--- tests/testthat/_snaps/github.md
+++ tests/testthat/_snaps/github.md
@@ -1,4 +1,4 @@
-# use_github_action_pkgcheck [plain]
+# use_github_action_pkgcheck
 
     `file_name` must be a character argument
 
@@ -9,40 +9,4 @@
 ---
 
     `inputs` must be a named list!
-
-# use_github_action_pkgcheck [ansi]
-
-    [1m[22m`file_name` must be a character argument
-
----
-
-    [1m[22m`file_name` must be a single value
-
----
-
-    [1m[22m`inputs` must be a named list!
-
-# use_github_action_pkgcheck [unicode]
-
-    `file_name` must be a character argument
-
----
-
-    `file_name` must be a single value
-
----
-
-    `inputs` must be a named list!
-
-# use_github_action_pkgcheck [fancy]
-
-    [1m[22m`file_name` must be a character argument
-
----
-
-    [1m[22m`file_name` must be a single value
-
----
-
-    [1m[22m`inputs` must be a named list!
 
--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -24,3 +24,26 @@ test_that ("use_github_action_pkgcheck", {
     expect_snapshot_file (use_github_action_pkgcheck (dir = dir, file_name = "with_inputs.yaml", inputs = list (`post-to-issue` = "true", `summary-only` = "false", ref = "main")))
 
 })
+
+test_that ("yaml branch", {
+
+    yaml <- system.file (
+        "pkgcheck.yaml",
+        package = "pkgcheck",
+        mustWork = TRUE
+    ) %>% readLines ()
+
+    expect_true (any (grepl ("^\\s+\\-\\s*main$", yaml)))
+
+    yaml1 <- add_branch_to_yaml (yaml)
+    expect_identical (yaml, yaml1)
+
+    yaml2 <- add_branch_to_yaml (yaml, branch = "new-branch")
+    expect_true (length (yaml2) > length (yaml1))
+    expect_equal (length (yaml2) - length (yaml1), 1L)
+    expect_true (any (grepl ("^\\s+\\-\\s*new-branch$", yaml2)))
+
+    pos1 <- grep ("^\\s+\\-\\s*main$", yaml)
+    pos2 <- grep ("^\\s+\\-\\s*new-branch$", yaml2)
+    expect_equal (pos2 - pos1, 1L)
+})
