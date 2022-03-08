dab3f9eec1a778b77c0ab4f9ba8f7a08102ff8ac
mpadge <mark.padgham@email.com>
2022-03-08 13:46:06 +1100

    fix test-github for new fs paths

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -1,30 +1,24 @@
 cli::test_that_cli ("use_github_action_pkgcheck", {
-    dir <- file.path (tempdir (), basename (tempfile ("pkgcheck")), ".github")
+
+    dir <- fs::path (fs::file_temp (pattern = "pkgcheck"), ".github")
     # nolint start
     expect_snapshot_error (use_github_action_pkgcheck (file_name = 23))
     expect_snapshot_error (use_github_action_pkgcheck (file_name = c ("some", "files")))
 
     dir.create (dir, recursive = TRUE)
-    path <- file.path (dir, "pkgcheck.yaml")
+    path <- fs::path (dir, "pkgcheck.yaml")
     file.create (path)
     expect_error (use_github_action_pkgcheck (dir = dir), "already exists")
     file.remove (path)
     expect_snapshot_error (use_github_action_pkgcheck (dir = dir, inputs = "not a list"))
     expect_error (use_github_action_pkgcheck (dir = dir, inputs = list (notaninput = 23)), "not valid")
 
-    # Success
-    # macOS GitHub runners append "/private" to paths when they are actually created:
-    gha_path <- gsub ("^\\/private", "", use_github_action_pkgcheck (dir = dir))
-    # paths on mac also use one double-separator between machine and Rtemp dirs:
-    expect_equal (
-        gsub ("\\/\\/", "/", gha_path),
-        gsub ("\\/\\/", "/", path)
-    )
-    gha_path <- gsub ("^\\/private", "", use_github_action_pkgcheck (dir = dir, overwrite = TRUE))
-    expect_equal (
-        fs::path_tidy (gha_path),
-        fs::path_tidy (path)
-    )
+    # Success - but skip on windows because GHA machines use completely
+    # different paths
+    testthat::skip_on_os ("windows")
+    gha_path <- use_github_action_pkgcheck (dir = dir)
+    expect_equal (path, gha_path)
+
     expect_snapshot_file (path)
     expect_snapshot_file (use_github_action_pkgcheck (dir = dir, file_name = "with_inputs.yaml", inputs = list (`post-to-issue` = "true", `summary-only` = "false", ref = "main")))
 
