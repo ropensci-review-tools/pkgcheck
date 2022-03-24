c9d081257457585a9d6666d98fab860d4a972129
mpadge <mark.padgham@email.com>
2022-03-24 15:16:40 +0100

    add get_re_exports fn

--- R/utils.R
+++ R/utils.R
@@ -104,3 +104,55 @@ env2namespace <- function (e) {
 
     return (e)
 }
+
+#' Get re-exported functions
+#'
+#' Note that import & export statements can span multiple lines, and
+#' `importFrom` can accept multiple fns.
+#'
+#' @param path Local path to package source
+#' @return A `data.frame` of [package, fn] detailing original source of all
+#' re-exported functions.
+#' @noRd
+get_re_exports <- function (path) {
+
+    nmsp <- readLines (file.path (path, "NAMESPACE"))
+
+    op <- grep ("\\(", nmsp)
+    cl <- grep ("\\)", nmsp)
+    if (length (op) != length (cl)) {
+        stop ("'NAMESPACE' file is incorrectly formatted")
+    }
+    index <- cbind (rev (op), rev (cl))
+    if (any ((cl - op) > 1L)) {
+        nmsp <- apply (index, 1, function (i)
+                       paste0 (nmsp [seq (i [1], i [2])], collapse = ""))
+    }
+
+    imports <- grep ("^importFrom", nmsp, value = TRUE)
+    import_fns <- gsub ("^importFrom\\(|\\)$", "", imports)
+    import_pkgs <- vapply (import_fns, function (i)
+                           strsplit (i, ",") [[1]] [1],
+                           character (1),
+                           USE.NAMES = FALSE)
+    import_pkgs <- gsub ("\"", "", import_pkgs)
+    import_fns <- lapply (strsplit (import_fns, ","), function (i) i [-1])
+    import_fns <- lapply (seq_along (import_pkgs), function (i)
+                          cbind (rep (import_pkgs [[i]],
+                                      length (import_fns [[i]])),
+                                 import_fns [[i]]))
+    import_fns <- do.call (rbind, import_fns)
+    import_fns <- data.frame (package = import_fns [, 1],
+                              fn = gsub ("^\\s*|#.*$", "", import_fns [, 2]))
+
+    exports <- grep ("^export\\(", nmsp, value = TRUE)
+    exports <- gsub ("^export\\(|\\)", "", exports)
+    exports <- unlist (strsplit (exports, ","))
+    exports <- gsub ("^\\s*|\\t", "", exports)
+    exports <- gsub ("#.*$", "", exports)
+
+    re_exports <- exports [which (exports %in% import_fns$fn)]
+    re_exports <- import_fns [match (re_exports, import_fns$fn), ]
+
+    return (re_exports)
+}
