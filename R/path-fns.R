4dbc3c81d68402bf92937cd2b2bcdf115d406762
mpadge <mark.padgham@email.com>
2024-11-11 12:54:15 +0100

    allow pkgs in sub-directories for ropensci-review-tools/roreviewapi#64

--- R/path-fns.R
+++ R/path-fns.R
@@ -1,4 +1,3 @@
-
 #' Check and convert path arguments
 #'
 #' Mostly called for side-effects of erroring when path is not root directory of
@@ -20,6 +19,26 @@ convert_path <- function (path = ".") {
 
     if (is.null (proj_root)) {
         stop (paste0 ("[", path, "] does not appear to be an R package"))
+    }
+
+    proj_root <- tryCatch (
+        rprojroot::find_package_root_file (path = path),
+        error = function (e) NULL
+    )
+    if (is.null (proj_root)) {
+        subdirs <- fs::dir_ls (path, type = "directory")
+        proj_root <- unlist (lapply (subdirs, function (d) {
+            tryCatch (
+                rprojroot::find_package_root_file (path = d),
+                error = function (e) NULL
+            )
+        }))
+    }
+
+    if (length (proj_root) != 1L) {
+        cli::cli_abort (
+            "Could not find unambiguous project root from {proj_root}"
+        )
     }
 
     return (proj_root)
