495aa7028cf1e3c14472c81ed1139cbc193e1719
mpadge <mark.padgham@email.com>
2024-11-19 09:54:26 +0100

    improve convert_path fn to detect nested sub-dirs

--- R/path-fns.R
+++ R/path-fns.R
@@ -1,4 +1,3 @@
-
 #' Check and convert path arguments
 #'
 #' Mostly called for side-effects of erroring when path is not root directory of
@@ -9,18 +8,39 @@ convert_path <- function (path = ".") {
     path <- fs::path_norm (path)
 
     # see also https://github.com/r-lib/usethis/blob/master/R/proj.R
-    proj_crit <- function () {
-        rprojroot::is_r_package |
-            rprojroot::is_git_root
-    }
-    proj_root <- tryCatch (
-        rprojroot::find_root (proj_crit (), path = path),
+    git_root <- tryCatch (
+        rprojroot::find_root (rprojroot::is_git_root, path = path),
         error = function (e) NULL
     )
 
-    if (is.null (proj_root)) {
-        stop (paste0 ("[", path, "] does not appear to be an R package"))
+    if (!is.null (git_root)) {
+        path <- git_root
     }
 
-    return (proj_root)
+    r_root <- tryCatch (
+        rprojroot::find_root (rprojroot::is_r_package, path = path),
+        error = function (e) NULL
+    )
+    if (is.null (r_root)) {
+
+        subdirs <- fs::dir_ls (path, type = "directory")
+        r_root <- vapply (subdirs, function (d) {
+            tryCatch (
+                rprojroot::find_root (rprojroot::is_r_package, path = d),
+                error = function (e) ""
+            )
+        }, character (1L), USE.NAMES = FALSE)
+        r_root <- r_root [which (nzchar (r_root))]
+
+        if (length (r_root) != 1L) {
+            cli::cli_abort (paste0 (
+                "Unable to determine unambiguous ",
+                "R source directory from [{r_root}]"
+            ))
+        }
+    }
+
+
+
+    return (r_root)
 }
