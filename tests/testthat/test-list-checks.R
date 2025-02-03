b13b721ed78991bd740d9a9dc9c7d4b038d9e8ac
mpadge <mark.padgham@email.com>
2025-02-03 10:42:28 +0100

    update expected number of checks in tests for #215

--- tests/testthat/test-list-checks.R
+++ tests/testthat/test-list-checks.R
@@ -1,10 +1,9 @@
-
 test_that ("list-checks", {
     expect_message (
         chks <- list_pkgchecks (),
         "The following checks are currently implemented"
     )
-    expect_length (chks, 19L)
+    expect_length (chks, 20L)
 
     expect_silent (
         chks2 <- list_pkgchecks (quiet = TRUE)
