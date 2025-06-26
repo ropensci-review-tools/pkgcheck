17b5da82fccad876540b7541e6286e1bd2c4c360
mpadge <mark.padgham@email.com>
2025-06-26 15:21:38 +0200

    add test-check-pkgname-available for #256

---
+++ tests/testthat/test-check-pkgname-available.R
@@ -0,0 +1,23 @@
+test_that ("check num imports", {
+
+    checks <- make_check_data ()
+
+    ci_out <- output_pkgchk_pkgname (checks)
+
+    expect_true (ci_out$check_pass)
+    expect_length (ci_out$summary, 1L)
+    expect_equal (
+        ci_out$summary,
+        "Package name is available"
+    )
+    expect_length (ci_out$print, 1L)
+    expect_false (nzchar (ci_out$print))
+
+    checks$checks$pkgname_available <- FALSE
+    ci_out <- output_pkgchk_pkgname (checks)
+    expect_false (ci_out$check_pass)
+    expect_equal (
+        ci_out$summary,
+        "Package name is not available (on CRAN)."
+    )
+})
