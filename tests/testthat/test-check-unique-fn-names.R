e705b8ce1dd3bff4f51bdc168613eb5b48d75711
mpadge <mark.padgham@email.com>
2025-06-26 17:06:56 +0200

    add test-check-unique-fn-names.R for #256

---
+++ tests/testthat/test-check-unique-fn-names.R
@@ -0,0 +1,29 @@
+test_that ("check unique functions names", {
+
+    checks <- make_check_data ()
+
+    ci_out <- output_pkgchk_unique_fn_names (checks)
+    expect_type (ci_out, "list")
+    expect_length (ci_out, 3L)
+    expect_named (ci_out, c ("check_pass", "summary", "print"))
+
+    expect_true (ci_out$check_pass)
+    expect_length (ci_out$summary, 1L)
+    expect_false (nzchar (ci_out$summary))
+    expect_length (ci_out$print, 1L)
+    expect_false (nzchar (ci_out$print))
+
+    dat <- data.frame (
+        package = c ("one", "two"),
+        version = 1:2,
+        fn_name = c ("yes", "no")
+    )
+    checks$checks$unique_fn_names <- dat
+    ci_out <- output_pkgchk_unique_fn_names (checks)
+    expect_false (ci_out$check_pass)
+    expect_equal (
+        ci_out$summary,
+        "Function names are duplicated in other packages"
+    )
+    expect_identical (sort (names (ci_out$print$obj)), sort (dat$fn_name))
+})
