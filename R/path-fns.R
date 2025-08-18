94e1ff85d50f365f8d0470d93ca1149e7d218baa
Brock <kbrock@ab.mpg.de>
2025-08-18 16:24:52 +0200

    small refactor

--- R/path-fns.R
+++ R/path-fns.R
@@ -4,38 +4,34 @@
 #' an R package.
 #' @noRd
 convert_path <- function (path = ".") {
+    x_if_not_null <- function(x, y) if (is.null(x)) y else x
 
     path <- fs::path_norm (path)
 
     # see also https://github.com/r-lib/usethis/blob/master/R/proj.R
-    git_root <- tryCatch (
+    path <- x_if_not_null(tryCatch (
         rprojroot::find_root (rprojroot::is_git_root, path = path),
         error = function (e) NULL
-    )
+    ), path)
 
-    if (!is.null (git_root)) {
-        path <- git_root
-    }
-
-    proj_root <- tryCatch (
-        rprojroot::find_package_root_file (path = path),
-        error = function (e) NULL
-    )
-    if (is.null (proj_root)) {
-        subdirs <- fs::dir_ls (path, type = "directory")
-        proj_root <- unlist (lapply (subdirs, function (d) {
+    path <- x_if_not_null(
+        tryCatch (
+            rprojroot::find_package_root_file (path = path),
+            error = function (e) NULL
+        ),
+        unlist (lapply (fs::dir_ls (path, type = "directory"), function (d) {
             tryCatch (
                 rprojroot::find_root (rprojroot::is_r_package, path = d),
                 error = function (e) NULL
             )
         }))
-    }
+    )
 
-    if (length (proj_root) != 1L) {
+    if (length (path) != 1L) {
         cli::cli_abort (
-            "Could not find unambiguous project root from {proj_root}"
+            "Could not find unambiguous project root from {path}"
         )
     }
 
-    return (fs::path (proj_root))
+    return (fs::path (path))
 }
