303af089d874602e6231e16500ede5982c5f7980
Andy Teucher <andy.teucher@gmail.com>
2025-08-27 16:56:43 -0700

    Test check_uses_dontrun and internal function

--- tests/testthat/test-check-uses-dontrun.R
+++ tests/testthat/test-check-uses-dontrun.R
@@ -14,23 +14,40 @@ test_that ("check examples dont use dontrun", {
       "All examples use `\\dontrun{}`."
   )
   expect_length (ci_out$print, 1L)
-  expect_true (nzchar (ci_out$print))
+  expect_snapshot (ci_out$print)
 
   # Some examples use `\dontrun{}`
-  checks$checks$uses_dontrun ["pkgstats_summary"] <- FALSE
+  checks$checks$uses_dontrun ["pkgstats_summary"] <- "some"
   ci_out <- output_pkgchk_uses_dontrun (checks)
 
   expect_false (ci_out$check_pass)
   expect_equal (
     ci_out$summary, 
-    "Examples should not use `\\dontrun{{}}` unless really necessary."
+    "Examples should not use `\\dontrun{}` unless really necessary."
+  )
+  expect_match(
+    ci_out$print, 
+    "The following functions have examples that use `\\dontrun{}`", 
+    fixed = TRUE
   )
   
   # No examples using `\dontrun{}`
-  checks$checks$uses_dontrun[] <- FALSE
+  checks$checks$uses_dontrun[] <- "none"
   ci_out <- output_pkgchk_uses_dontrun (checks)
 
   expect_true (ci_out$check_pass)
   expect_equal (ci_out$summary, "")
   expect_equal (ci_out$print, "")
+})
+
+test_that ("uses_dontrun works", {
+
+  rd_files <- system.file("Rd-for-testing-dontrun.Rd", package = "pkgcheck")
+
+  uses_dontrun_output <- rd_files_use_dontrun (rd_files)
+
+  expect_type (uses_dontrun_output, "character")
+  expect_length (uses_dontrun_output, 1L)
+  expect_named (uses_dontrun_output, "example_function")
+  expect_equal (uses_dontrun_output, c(example_function = "some"))
 })
