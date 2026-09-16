5866a2cc642675af3b12cc6109f8f72e2632d558
mpadge <mark.padgham@email.com>
2026-09-16 10:20:49 +0200

    add a test to ensure all checks are listed in the order checks fn

--- tests/testthat/test-list-checks.R
+++ tests/testthat/test-list-checks.R
@@ -10,3 +10,21 @@ test_that ("list-checks", {
     )
     expect_identical (chks, chks2)
 })
+
+# Every new function **MUST** be added to 'order_checks_sequence' defined in
+# 'R/order-check-summaries.R'. This test confirms that.
+test_that ("All checks listed in 'order_checks_sequence'", {
+
+    # List all output fns:
+    ptn <- "^output\\_pkgchk\\_"
+    fns <- grep (
+        ptn,
+        ls (envir = asNamespace ("pkgcheck"), all.names = TRUE),
+        value = TRUE
+    )
+    fns <- sort (gsub (ptn, "", fns))
+
+    ords <- sort (order_checks_sequence)
+
+    expect_identical (fns, ords)
+})
